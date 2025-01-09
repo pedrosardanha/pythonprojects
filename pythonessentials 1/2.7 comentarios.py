d92e5ff228bd568e2dba315692f66cb6322da50f
Pedro Sardanha <ph264915@gmail.com>
2025-01-09 11:59:06 -0300

    Módulo 2 completo

---
+++ pythonessentials 1/2.7 comentarios.py
@@ -0,0 +1,11 @@
+#esse programa calcula o número de segundos em um dado número de horas
+# esse programa foi escrito a dois dias atrás
+
+a = 2 # número de horas
+seconds = 3600 # número de segundos em uma hora
+
+print("Horas: ", a) #imprimindo o número de horas
+# print("Seconds in Hours: ", a * seconds) # imprimindo o número de segundos em uma dada hora
+
+#aqui também devería escrever "Adeus", mas o programador não teve tempo de escrever código
+#esse é o final do programa que computa o número de segundos em 3 horas
